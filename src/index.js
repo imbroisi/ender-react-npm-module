410bf64a9c13305a6bdf17213f5edb15e77cb370
Ender <luizimbroisi@gmail.com>
2019-05-30 14:13:13 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import 'style.css';
+
+export default class DummyComponent extends Component {
+
+    render () {
+
+        return (
+            <div className="enderClass">Eu sou um módulo do Ender!</div>
+        )
+
+    }
+
+}
